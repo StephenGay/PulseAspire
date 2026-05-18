0baa5593e40561c991c95a63ba0459af2e7ed32e
Stephen Gay <stephen@hmrollers.com>
2026-05-18 10:40:17 +0200

    BU WIP 180526

--- Pulse.Web/Services/Global_AI_Functions.cs
+++ Pulse.Web/Services/Global_AI_Functions.cs
@@ -7,9 +7,6 @@ public class Global_AI_Functions
     public event Func<string, string, Task>? OnAISpeakRequested;
     public Task AISpeak(string text, string voice) => OnAISpeakRequested != null ? OnAISpeakRequested.Invoke(text, voice) : Task.CompletedTask;
 
-    //public async Task StopSpeaking()
-    //{
-    //    await SpeechSynthesis.CancelAsync();
-    //}
+    
 }
 
